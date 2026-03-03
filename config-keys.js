67bb81c707796f7e0c8e32e97ae581e3beac3417
alissaj1 <ji.alissa@gmail.com>
2026-03-03 12:21:36 -0800

    Demo only: expose API key for Vercel static deploy

---
+++ config-keys.js
@@ -0,0 +1,5 @@
+// This file contains your API key
+// It is gitignored and will NOT be pushed to GitHub
+const CONFIG_KEYS = {
+  OPENAI_API_KEY: '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'
+};
